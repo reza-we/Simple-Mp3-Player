3f58eb4ce6a50495a4cecf18ef51089a272bda74
reza-we <s.reza.we@gmail.com>
2025-03-14 00:35:28 +0330

    making general form of the app and the frame

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ except:
 app = C.CTk()
 app.geometry("700x400")
 app.title("Mp3 player")
+app.configure(fg_color="#33001a")
+
+lframe = C.CTkFrame(app , 160 , 400 ,fg_color="#660033", border_width=2 ,
+     border_color= "#b30059" ,corner_radius=15)
+lframe.place(x = 1 , y = 1  )
+
 app.mainloop()
